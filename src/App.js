7d3bbde0f386d4b450923540c49344c56cb14ac3
MohammadHossein <m.h.dolatabadi.a@gmail.com>
2021-12-15 17:55:19 +0330

    feat: Add projects section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import {
   AppBar,
   Avatar,
   Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
   Stack,
   Tab,
   Tabs,
@@ -10,6 +16,142 @@ import {
 } from '@mui/material'
 import { Box } from '@mui/system'
 import { useState } from 'react'
+import CodeIcon from '@mui/icons-material/Code'
+
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+
+const StyledTabs = styled((props) => (
+  <Tabs
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+  />
+))({
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  '& .MuiTabs-indicatorSpan': {
+    maxWidth: 40,
+    width: '100%',
+    backgroundColor: '#2c3e50',
+  },
+})
+
+const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: 'none',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    color: 'rgba(255, 255, 255, 0.7)',
+    '&.Mui-selected': {
+      color: '#fff',
+    },
+    '&.Mui-focusVisible': {
+      backgroundColor: 'rgba(100, 95, 228, 0.32)',
+    },
+  })
+)
+
+const styles = (theme) => ({
+  root: {
+    padding: 0,
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+
+  item: {
+    padding: 0,
+  },
+})
+
+export function CustomizedTabs() {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Box>
+      <Box disablePadding>
+        <StyledTabs
+          value={value}
+          onChange={handleChange}
+          aria-label="styled tabs example"
+        >
+          <StyledTab label="Projects" />
+          <StyledTab label="Courses" />
+          <StyledTab label="Skills" />
+        </StyledTabs>
+      </Box>
+      <TabPanel disablePadding value={value} index={0} sx={{ padding: 0 }}>
+        <List disablePadding className={styles.root}>
+          <ListSubheader sx={{ bgcolor: 'transparent', color: '#2c3e50' }}>
+            Personal Projects
+          </ListSubheader>
+          {[
+            { primary: 'Chat Application', secondary: 'written in C' },
+            { primary: 'Chicken Invaders', secondary: 'written in Java' },
+            { primary: 'C-minus Compiler', secondary: 'written in Python' },
+            {
+              primary: 'Dots and Boxes',
+              secondary: 'written in JS(using react)',
+            },
+          ].map(({ primary, secondary }) => (
+            <ListItem disableGutters sx={{ padding: 0 }}>
+              <ListItemIcon>
+                <CodeIcon sx={{ color: '#ffffff', p: 0, m: 0 }} />
+              </ListItemIcon>
+              <ListItemText
+                disablePadding
+                primary={primary}
+                secondary={secondary}
+                secondaryTypographyProps={{
+                  color: 'rgba(255, 255, 255, 0.7)',
+                }}
+              />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List disablePadding>
+          <ListSubheader sx={{ bgcolor: 'transparent', color: '#2c3e50' }}>
+            Team Projects
+          </ListSubheader>
+          {[
+            { primary: 'LearnX', secondary: 'written in Java(Android)' },
+            { primary: 'Online Store', secondary: 'written in Java' },
+            { primary: 'ToDo', secondary: 'written in Swift' },
+          ].map(({ primary, secondary }) => (
+            <ListItem disableGutters sx={{ padding: 0 }}>
+              <ListItemIcon>
+                <CodeIcon sx={{ color: '#ffffff', p: 0, m: 0 }} />
+              </ListItemIcon>
+              <ListItemText
+                disablePadding
+                primary={primary}
+                secondary={secondary}
+                secondaryTypographyProps={{
+                  color: 'rgba(255, 255, 255, 0.7)',
+                }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </TabPanel>
+      <TabPanel value={value} index={1} sx={{ padding: 0 }}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2} sx={{ padding: 0 }}>
+        Item Three
+      </TabPanel>
+    </Box>
+  )
+}
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props
@@ -31,13 +173,6 @@ function TabPanel(props) {
   )
 }
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  }
-}
-
 function App() {
   const [value, setValue] = useState()
   const handleChange = (event, value) => {
@@ -53,26 +188,7 @@ function App() {
           sx={{ width: 100, height: 100 }}
         />
         <Typography>MohammadHossein Dolatabadi</Typography>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            <Tab label="Projects" {...a11yProps(0)} />
-            <Tab label="Skills" {...a11yProps(1)} />
-            <Tab label="Courses" {...a11yProps(2)} />
-          </Tabs>
-        </Box>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Item Three
-        </TabPanel>
+        <CustomizedTabs />
       </Stack>
     </div>
   )
