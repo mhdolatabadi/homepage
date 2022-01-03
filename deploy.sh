15da4df1922b55068977864cf59c738ce9e81d25
MohammadHossein <m.h.dolatabadi.a@gmail.com>
2022-01-03 21:10:52 +0330

    fix: Responsive designs

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 rm -rf home_page.tar.gz
 echo "Under Deploy..."
 CE_USERNAME=mhdolatabadia
-ls -lh >> lists.txt
 tar -czvf home_page.tar.gz * 
 sudo scp home_page.tar.gz $CE_USERNAME@ce.sharif.edu:public_html/
+rm -rf *.tar.gz
