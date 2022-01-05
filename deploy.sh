57b22126bab4b88f83a7a08b17ba31fa190027a2
MohammadHossein <m.h.dolatabadi.a@gmail.com>
2022-01-05 18:09:18 +0330

    Jump to react

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf home_page.tar.gz
-echo "Under Deploy..."
-CE_USERNAME=mhdolatabadia
-tar -czvf home_page.tar.gz * 
-sudo scp home_page.tar.gz $CE_USERNAME@ce.sharif.edu:public_html/
-rm -rf *.tar.gz
