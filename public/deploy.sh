34bcc90022ff4a34c0d765b19c5363c3573e82c1
mohammadhossein <m.h.dolatabadi.a@gmail.com>
2022-07-01 12:50:56 +0430

    fix(deploy): Fix deploy script according to new host requirement
    - Fix footer style

--- public/deploy.sh
+++ public/deploy.sh
@@ -3,5 +3,5 @@ rm -rf home_page.tar.gz
 echo "Under Deploy..."
 CE_USERNAME=mhdolatabadia
 tar -czvf home_page.tar.gz ./*
-sudo scp -F ~/.ssh/config home_page.tar.gz $CE_USERNAME@ce.sharif.edu:public_html/ -F
+sudo scp -oHostKeyAlgorithms=+ssh-rsa home_page.tar.gz $CE_USERNAME@ce.sharif.edu:public_html/
 rm -rf ./*.tar.gz
