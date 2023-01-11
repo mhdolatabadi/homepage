cbc6eb92851d69bb01604f0606f526cbeff750a9
Mohammadhossein <m.h.dolatabadi.a@gmail.com>
2023-01-11 17:28:18 +0330

    Add build folder for github pages

---
+++ build/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf home_page.tar.gz
+echo "Under Deploy..."
+CE_USERNAME=mhdolatabadia
+tar -czvf home_page.tar.gz ./*
+sudo scp -oHostKeyAlgorithms=+ssh-rsa home_page.tar.gz $CE_USERNAME@ce.sharif.edu:public_html/
+rm -rf ./*.tar.gz
